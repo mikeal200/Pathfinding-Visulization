dc4faf5b8e8cf33de3cbf5015fdb5a8f19b82482
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-08-03 12:02:21 -0400

    added button and correct JFrame size

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.*;
@@ -7,18 +8,32 @@ public class Main extends JPanel {
 
 	public static int cols = 25;
 	public static int rows = 25;
+	public static int tileWidth = 800 / cols;
+	public static int tileHeight = 600 / rows;
 	public static Spot[][] grid = new Spot[cols][rows];
 	public static ArrayList<Spot> openSet = new ArrayList<>();
 	public static ArrayList<Spot> closedSet = new ArrayList<>();
 	public static ArrayList<Spot> path;
-	public static ArrayList<Spot> walls = new ArrayList<>();;
+	public static ArrayList<Spot> walls = new ArrayList<>();
 	private static boolean done = false;
+	private static int col = 0;
+	private static int row = 0;
+	public static Spot start = null;
+	public static Spot end = null;
+	private static boolean startB = false;
+	private static boolean endB = false;
+	private static boolean wallB = false;
+	private static int clicks = 0;
 
 	public static void main(String[] args) {
 		Main mainPanel  = new Main();
 		JFrame frame = new JFrame("Pathfinder");
-		frame.setSize(800, 600);
+		JButton goButton = addNewButton();
+		//goButton.setBounds(750, 570, 60, 40);
+		frame.getContentPane().add(new Main());
+		frame.setSize(816, 639);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(goButton);
 		frame.add(mainPanel);
 		frame.setVisible(true);
 
@@ -43,19 +58,36 @@ public class Main extends JPanel {
 		openSet.add(start);
 	}
 
+	//Create button and action listener:
+    public static JButton addNewButton(){
+        JButton button = new JButton("Next");
+        button.setBounds(700, 550, 60, 40);
+        button.addActionListener(new java.awt.event.ActionListener(){
+        @Override
+        public void actionPerformed(java.awt.event.ActionEvent evt){
+            buttonAction(evt);
+        }
+        });
+        return button;
+	}
+	
+	//Action fer each button:
+    public static void buttonAction(ActionEvent e) {
+		System.out.println("button worked");
+    }
+
 	public void paintComponent (Graphics g) {
 		Spot current = null;
+		boolean choosen = false;
 		super.paintComponent(g);
-		Spot start = grid[1][1];
-		Spot end = grid[3][7];
 
-		walls.add(grid[2][2]);
+		/*walls.add(grid[2][2]);
 		walls.add(grid[2][1]);
 		walls.add(grid[2][3]);
 		walls.add(grid[2][4]);
 		walls.add(grid[2][5]);
 		walls.add(grid[2][6]);
-		walls.add(grid[2][7]);
+		walls.add(grid[2][7]);*/
 
 		for (int i = 0; i < cols; i++) {
 			for (int j = 0; j < rows; j++) {
@@ -63,87 +95,103 @@ public class Main extends JPanel {
 			}
 		}
 
-		if(openSet.size() > 0) {
-			int lowestCost = 0;
+		//write something that ends this nonsense
 
-			for(int i = 0; i < openSet.size(); i++) {
-				if(openSet.get(i).f < openSet.get(lowestCost).f) {
-					lowestCost = i;
-				}
+		if(start != null) {
+			start.draw(Color.PINK, g);
+		}
+		if(end != null) {
+			end.draw(Color.PINK, g);
+		}
+		if(walls.size() > 0) {
+			for (int i = 0; i < walls.size(); i++) {
+				walls.get(i).draw(Color.BLACK, g);
 			}
-			current = openSet.get(lowestCost);
+		}
 
-			if(current.getX() == end.getX() && current.getY() == end.getY()) {			
-				System.out.println("done");
-				done = true;
-			}
+		if(choosen) {
+			if(openSet.size() > 0) {
+				int lowestCost = 0;
 
-			removeSpot(current);
-			closedSet.add(current);
-
-			ArrayList<Spot> neighbors = current.neighbors;
-			for(int i = 0; i < neighbors.size(); i++) {
-				Spot neighbor = neighbors.get(i);
-				if(walls.contains(neighbor)){
-					continue;
+				for(int i = 0; i < openSet.size(); i++) {
+					if(openSet.get(i).f < openSet.get(lowestCost).f) {
+						lowestCost = i;
+					}
 				}
-				else if(!closedSet.contains(neighbor)){
-					double tempG = current.g + 1;
-					if(openSet.contains(neighbor)) {
-						if(tempG < neighbor.g) {
-							neighbor.g = tempG;
+				current = openSet.get(lowestCost);
+
+				if(current.getX() == end.getX() && current.getY() == end.getY()) {			
+					System.out.println("done");
+					done = true;
+				}
+
+				removeSpot(current);
+				closedSet.add(current);
+
+				ArrayList<Spot> neighbors = current.neighbors;
+				for(int i = 0; i < neighbors.size(); i++) {
+					Spot neighbor = neighbors.get(i);
+					if(walls.contains(neighbor)){
+						continue;
+					}
+					else if(!closedSet.contains(neighbor)){
+						double tempG = current.g + 1;
+						if(openSet.contains(neighbor)) {
+							if(tempG < neighbor.g) {
+								neighbor.g = tempG;
+							}
 						}
+						else {
+							neighbor.g = tempG;
+							openSet.add(neighbor);
+						}
+						neighbor.previous = current;
+						neighbor.h = heurisitic(neighbor, end);
+						neighbor.f = neighbor.g + neighbor.h;
 					}
-					else {
-						neighbor.g = tempG;
-						openSet.add(neighbor);
-					}
-					neighbor.previous = current;
-					neighbor.h = heurisitic(neighbor, end);
-					neighbor.f = neighbor.g + neighbor.h;
 				}
 			}
-		}
 
-		for (int i = 0; i < walls.size(); i++) {
-			walls.get(i).draw(Color.BLACK, g);
-		}
-
-		for (int i = 0; i < closedSet.size(); i++) {
-			closedSet.get(i).draw(Color.RED, g);
-		}
-
-		for (int i = 0; i < openSet.size(); i++) {
-			openSet.get(i).draw(Color.GREEN, g);
-		}
-
-		path = new ArrayList<>();
-		Spot temp = current;
-		path.add(temp);
-		while(temp.previous != null) {
-			path.add(temp.previous);
-			temp = temp.previous;
-		}
-
-		if(path.size() > 0) {
-			for(int i = 0; i < path.size(); i++) {
-				path.get(i).draw(Color.BLUE, g);
+			for (int i = 0; i < walls.size(); i++) {
+				walls.get(i).draw(Color.BLACK, g);
 			}
-		}
 
-		try {
-			Thread.sleep(50);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+			for (int i = 0; i < closedSet.size(); i++) {
+				closedSet.get(i).draw(Color.RED, g);
+			}
 
-		if(done){}
-		else{
-			repaint();
-		}
+			for (int i = 0; i < openSet.size(); i++) {
+				openSet.get(i).draw(Color.GREEN, g);
+			}
 
-		start.draw(Color.PINK, g);
-		end.draw(Color.PINK, g);
+			path = new ArrayList<>();
+			Spot temp = current;
+			path.add(temp);
+			while(temp.previous != null) {
+				path.add(temp.previous);
+				temp = temp.previous;
+			}
+
+			if(path.size() > 0) {
+				for(int i = 0; i < path.size(); i++) {
+					path.get(i).draw(Color.BLUE, g);
+				}
+			}
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if(done){}
+			else{
+				repaint();
+			}
+
+			start.draw(Color.PINK, g);
+			end.draw(Color.PINK, g);
+		}
 	}
 
 	public static void removeSpot(Spot elem) {
@@ -159,5 +207,30 @@ public class Main extends JPanel {
 	public static double heurisitic(Spot a, Spot b) {
 		double d = Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 		return d;
+	}
+
+	public Main() {
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				int x=e.getX();
+				int y=e.getY();
+				col = x / tileWidth;
+				row = y / tileHeight;
+				if(wallB) {
+					walls.add(grid[col][row]);
+				}
+				if(endB) {
+					end = grid[col][row];
+					endB = false;
+					wallB = true;
+				}
+				if(clicks == 0) {
+					endB = true;
+					clicks++;
+					start = grid[col][row];
+				}
+				repaint();
+			}
+		});
 	}
 }
